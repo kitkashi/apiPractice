7b7e8df561506c5c312fac57dfdfaad7882c42de
kitkashi <skit.edwards@gmail.com>
2026-06-25 14:34:54 -0500

    created data container and connected js

--- apiCall.js
+++ apiCall.js
@@ -1,1 +1,16 @@
-function apiCall();
+async function apiCall(){
+
+    //api endpoint that i will constantly call
+    const url='https://dog.ceo/api/breeds/image/random'
+
+    try{
+        const response = await fetch(url);
+        //if response not within 200-299 range throws error
+        if(!response.ok){
+            throw new Error(`HTTP Error! Status: ${response}`)
+        }
+
+    const data = response.json();
+
+    }
+}
